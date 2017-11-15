c58fded2600732da700b7efa289e24ae572cab9a
Mike Werling <mwerling@gmail.com>
2017-11-15 10:44:44 -0500

    Code cleanup

--- custom_regions/components/CustomRegionsMap.js
+++ custom_regions/components/CustomRegionsMap.js
@@ -12,107 +12,11 @@
     Help out the administrator who is configuring the map.  Make sure that all of the right
     variables are present and declared.
     */
-
-    //Omnivore is not loading in Microsoft Edge from the libraries collection in the custom chart.
-    //try loading as a script tag
-    if (typeof(omnivore) === 'undefined' || !omnivore) {
-        var omnivoreScriptBlock = document.createElement('script');
-        omnivoreScriptBlock.src = 'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-omnivore/v0.3.1/leaflet-omnivore.min.js';
-        omnivoreScriptBlock.type = 'text/javascript';
-        omnivoreScriptBlock.onload = function() {
-            console.log('Omnivore loaded ', typeof(omnivore));
-            initChart();
-        };
-        controller.element.appendChild(omnivoreScriptBlock);
-    } else {
-        initChart();
-    }
-
-    var dataLookup = {}; //this will contain the results from Zoomdata
-    var map = undefined;
-
-    try {
-        var userVariables = JSON.parse(controller.variables['Map Configuration']);
-    } catch (e) {
-        console.error('CR: Unable to parse configuration string.  Make sure the confiuration string contains well formatted JSON');
-        console.error(e.message);
-        console.error('CR: Configuration string is:', controller.variables['Map Configuration']);
-        return;
-    }
-
-    function initChart() {
-      // The administrator creating this visualization goes to the chart Configuration
-      //associated with the data source and
-
-      //This is just to help developers/admins to know when there is a problem with the config
-      validationResult = validateVariables(userVariables);
-      if (!validationResult.result) {
-          console.error('CR: Error in the map configuration variables');
-          console.error(validationResult.message);
-          console.error('CR: configuration variable:', userVariables);
-      }
-      //Example setting the tile server parameters manually, in this case for OpenStreetMap Mapnik
-      //userVariables.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      //	maxZoom: 19,
-      //	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      //});
-
-      /*
-      ****************
-      End User Customization Section
-      Everything from here down is visualization logic
-      ****************
-      */
-
-
-      // create a div for the map and add the leaflet map
-      var uuid = new Date().getTime();
-      var mapId = 'map-' + uuid;
-
-      var div = $(controller.element).append('<div id="' + mapId +
-          '" style="width:100%; height:100%" />').find(mapId).first();
-      $(div).addClass('map');
-
-      var maxBounds = new L.LatLngBounds(
-          new L.LatLng(userVariables.bounds.northEast.lat, userVariables.bounds.northEast.lon),
-          new L.LatLng(userVariables.bounds.southWest.lat, userVariables.bounds.southWest.lon)
-      );
-
-      if (map !== undefined) {
-          console.log('CR: Map exists ', map);
-      }
-      map = L.map(mapId, {
-          maxBounds: maxBounds,
-          minZoom: userVariables.bounds.minZoom
-      }).setView(userVariables.initialExtent.centerPoint,
-          userVariables.initialExtent.zoomLevel);
-
-      //var tileLayer = L.tileLayer.provider(userVariables.tileLayer.provider); //TODO: need to handle non-provider supported tile layers
-      // https: also suppported.
-      var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      });
-      tileLayer.addTo(map);
-      createCustomRegionLayers(userVariables.regionsConfig, map, style);
-      setCurrentLayer();
-      setLayerFilter();
-      map.on('moveend', function(e) {
-          //whenever the map is moved re-calculate the filter
-          setLayerFilter();
-      });
-
-      map.on('zoomend', function(e) {
-          setCurrentLayer();
-      });
-
-
-    } //end initChart
     var validateVariables = function(vars) {
         var result = true;
         var message = '';
         vars.regionsConfig.forEach(function(region) {
-            if (!window[region.regionData]) {
+            if(!window[region.regionData]) {
                 result = false;
                 message += 'Polygons for ' + vars.regionsConfig.regionData + ' not defined.  Load the polygons as a library in chart studio and make sure the variable name is correct';
             }
@@ -125,6 +29,67 @@
         };
     }
 
+    // The administrator creating this visualization goes to the chart Configuration
+    //associated with the data source and
+    try {
+        var userVariables = JSON.parse(controller.variables['Map Configuration']);
+    }
+    catch(e) {
+        console.error('CR: Unable to parse configuration string.  Make sure the confiuration string contains well formatted JSON');
+        console.error(e.message);
+        console.error('CR: Configuration string is:', controller.variables['Map Configuration']);
+        return;
+    }
+
+    //This is just to help developers/admins to know when there is a problem with the config
+    validationResult = validateVariables(userVariables);
+    if(!validationResult.result) {
+        console.error('CR: Error in the map configuration variables');
+        console.error(validationResult.message);
+        console.error('CR: configuration variable:', userVariables);
+    }
+    //Example setting the tile server parameters manually, in this case for OpenStreetMap Mapnik
+    //userVariables.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //	maxZoom: 19,
+    //	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    //});
+
+    /*
+    ****************
+    End User Customization Section
+    Everything from here down is visualization logic
+    ****************
+    */
+
+    var dataLookup = {}; //this will contain the results from Zoomdata
+
+    // create a div for the map and add the leaflet map
+    var uuid = new Date().getTime();
+    var mapId = 'map-' + uuid;
+
+    var div = $(controller.element).append('<div id="' + mapId +
+        '" style="width:100%; height:100%" />').find(mapId).first();
+    $(div).addClass('map');
+
+    var maxBounds = new L.LatLngBounds(
+        new L.LatLng(userVariables.bounds.northEast.lat, userVariables.bounds.northEast.lon),
+        new L.LatLng(userVariables.bounds.southWest.lat, userVariables.bounds.southWest.lon)
+    );
+
+    if(map !== undefined) {console.log('CR: Map exists ', map);}
+    var map = L.map(mapId, {
+        maxBounds: maxBounds,
+        minZoom: userVariables.bounds.minZoom
+    }).setView(userVariables.initialExtent.centerPoint,
+        userVariables.initialExtent.zoomLevel);
+
+    //var tileLayer = L.tileLayer.provider(userVariables.tileLayer.provider); //TODO: need to handle non-provider supported tile layers
+    // https: also suppported.
+    var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+    tileLayer.addTo(map);
 
     // Used when the view changes (zooming) to detect what region set to use
     function regionInZoomRange(region) {
@@ -132,14 +97,14 @@
         var zoomLevel = map.getZoom();
         var minZoomLevel = region.minZoomLevel || 0;
         var maxZoomLevel = region.maxZoomLevel || 18; //18 is the default max zoom for leaflet TileLayer
-        if (zoomLevel >= minZoomLevel && zoomLevel <= maxZoomLevel) {
+        if(zoomLevel >= minZoomLevel && zoomLevel <= maxZoomLevel) {
             result = true;
         }
-        return (result);
+        return(result);
     }
 
     function updateRegionFilter(region) {
-        console.log("CR: updating region filter ", region);
+        console.log("CR: updating region filter " , region);
         var newFilter = [];
         var badGeom = 0;
         region.mapLayer.eachLayer(function(l) {
@@ -147,14 +112,14 @@
             //there seems to be an issue with multipolygons, or some issue with the way we are
             //reducing the data or generating topojson.  The bounds for some features is coming back {}
             try {
-                if (map.getBounds().intersects(l.getBounds())) {
+                if( map.getBounds().intersects(l.getBounds()) ) {
                     newFilter.push(l.feature.properties[region.regionField]);
                 }
-            } catch (e) {
+            } catch(e) {
                 badGeom++;
             }
         });
-        if (badGeom > 0) console.error('CR: Found ', badGeom, ' invalid geometries when creating dynamic filter');
+        if(badGeom > 0) console.error('CR: Found ', badGeom, ' invalid geometries when creating dynamic filter');
         region.filter = {
             path: region.groupName,
             value: newFilter,
@@ -174,26 +139,18 @@
         updateRegionFilter(currRegion);
         currFilters = controller.query.filters.get();
         //first, if there is no filter on the query create one
-        if (currFilters.length !== 0) {
+        if( currFilters.length !== 0 ) {
             userVariables.regionsConfig.forEach(function(r) {
                 //some filter exists.  Search the list, remove any filters with
                 //path that matches a region grouping
-                //Find method not supported in IE
-                //var matchingFilter = currFilters.find(function(f) {
-                //    var result = false;
-                //    if (f.path === r.groupName) {
-                //        result = true;
-                //        controller.query.filters.removeFilters(f);
-                //    }
-                //    return result;
-                //});
-                var matchingFilter;
-                for(var i=0; i < currFilters.length; i++) {
-                    if(currFilters[i].path === r.groupName) {
-                        controller.query.filters.removeFilters(currFilters[i]);
-                        matchingFilter = currFilters[i];
+                var matchingFilter = currFilters.find(function(f) {
+                    var result = false;
+                    if(f.path === r.groupName) {
+                        result = true;
+                        controller.query.filters.removeFilters(f);
                     }
-                }
+                    return result;
+                });
             });
         }
         controller.query.filters.addFilters(currRegion.filter);
@@ -203,8 +160,8 @@
     //and associated grouping in Zoomdata query
     function setCurrentLayer() {
         userVariables.regionsConfig.forEach(function(currRegion) {
-            if (regionInZoomRange(currRegion)) {
-                if (!currRegion.visible) {
+            if(regionInZoomRange(currRegion)) {
+                if(!currRegion.visible) {
                     currRegion.visible = true;
                     currRegion.mapLayer.addTo(map);
                     var currGroup = controller.dataAccessors.region.getGroup();
@@ -215,7 +172,7 @@
             } else {
                 map.removeLayer(currRegion.mapLayer);
                 currRegion.visible = false;
-                if (controller.query.filters.length > 0) {
+                if(controller.query.filters.length > 0) {
                     controller.query.filters.removeFilters(currRegion.filter);
                 }
             }
@@ -224,21 +181,16 @@
 
     function getVisibleLayer() {
         //NOTE:  this only works for one visible layer.  If users need to have multiple layers showing simultaneously then we need to return an array or something
-        //Array.find not supported by IE
-        //var result = userVariables.regionsConfig.find(function(currRegion) {
-        var result = undefined;
-        for(var i=0; i< userVariables.regionsConfig.length; i++) {
-            var currRegion = userVariables.regionsConfig[i];
-            if (currRegion.visible !== undefined) {
-                if(currRegion.visible) result = currRegion;
+        var result = userVariables.regionsConfig.find(function(currRegion) {
+            if(currRegion.visible === undefined) {
+                return false;
             }
-
-
-        };
+            return currRegion.visible;
+        });
         return result;
     }
 
-    function getMetrics() {
+    function getMetrics()  {
         var dataAccessors = controller.dataAccessors;
         var metrics = {};
 
@@ -263,16 +215,13 @@
             fillOpacity: 0.7,
             fillColor: fillColor
         };
-
-        //var visibleRegion = userVariables.regionsConfig.find(function(currRegion) {
-        //    if (currRegion.visible === undefined) {
-         //       return false;
-         //   }
-        //    return currRegion.visible;
-        //});
-        var visibleRegion = getVisibleLayer();
-
-        if (visibleRegion) {
+        var visibleRegion = userVariables.regionsConfig.find(function(currRegion) {
+            if(currRegion.visible === undefined) {
+                return false;
+            }
+            return currRegion.visible;
+        });
+        if(visibleRegion) {
             id = feature.properties[visibleRegion.regionField]
 
             if (dataLookup && id in dataLookup) {
@@ -282,7 +231,7 @@
             //style depends on shape.  For lines we don't have a fill, just border.  Points
             //are circles, so they are treated same as polygons
 
-            switch (feature.geometry.type) {
+            switch(feature.geometry.type) {
                 case 'LineString':
                 case 'MultiLineString':
                     sym = {
@@ -311,11 +260,11 @@
         //Note, we are assuming that all features in a geojson are same type - not
         //mixing points with polygons, etc.
         regions.forEach(function(region) {
-            //console.log(mapId, 'CR: creating region:', window[region.regionData]);
+//console.log(mapId, 'CR: creating region:', window[region.regionData]);
             //Handle geojson different from topojson
-            switch (window[region.regionData].type) {
+            switch(window[region.regionData].type) {
                 case 'FeatureCollection':
-                    switch (window[region.regionData].features[0].geometry.type) {
+                    switch(window[region.regionData].features[0].geometry.type) {
                         case 'Polygon':
                         case 'MultiPolygon':
                             region.mapLayer = L.geoJson(window[region.regionData], {
@@ -330,7 +279,7 @@
                                     return L.circleMarker(latlng, style);
                                 },
                                 style: style,
-                                onEachFeature: onEachFeature
+                                onEachFeature:onEachFeature
                             });
                             break;
                         case 'LineString':
@@ -357,6 +306,17 @@
         });
     }
 
+    createCustomRegionLayers(userVariables.regionsConfig, map, style);
+    setCurrentLayer();
+    setLayerFilter();
+    map.on('moveend', function(e) {
+        //whenever the map is moved re-calculate the filter
+        setLayerFilter();
+    });
+
+    map.on('zoomend', function(e) {
+        setCurrentLayer();
+    });
 
     function highlightFeature(e) {
         var layer = e.target;
@@ -448,7 +408,7 @@
         }
 
         userVariables.regionsConfig.forEach(function(region) {
-            if (region.mapLayer !== undefined) {
+            if(region.mapLayer !== undefined) {
                 region.mapLayer.setStyle(style);
             }
         });
